00c7a0731c662ced597d7e5e01dac88b79821bb2
v.kapustyanyj <v.kapustyanyj@avvyland.com>
2022-10-18 15:17:58 +1000

    -add addressables -add init main app configs

---
+++ Assets/Scripts/ECS/Systems/InitializeGridSystem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Scripts.ECS.Systems
+{
+    public partial class InitializeGridSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/ECS/Systems/SystemGroups.cs
@@ -0,0 +1,7 @@
+using Unity.Entities;
+using UnityEngine;
+
+namespace Scripts.ECS
+{
+    public class InitSystemGroup: ComponentSystemGroup {}
+}
