b74e2a04e38231c2185859c4dffc660dbcba135a
v.kapustyanyj <supgoose@gmail.com>
2022-10-24 21:05:50 +1000

    - add walk/run animation

--- Assets/Scripts/ECS/Systems/AvatarMoveSystem.cs
+++ Assets/Scripts/ECS/Systems/AvatarMoveSystem.cs
@@ -41,8 +41,8 @@ namespace Scripts.ECS.Systems
                     var movement = new Vector3(0f, physicsVelocity.Linear.y, 0f) 
                                    + (Vector3.right * _joystickInputComponent.Horizontal * MainAppConfig.Instance.AvatarMoveSpeed)
                                    + (Vector3.forward * _joystickInputComponent.Vertical * MainAppConfig.Instance.AvatarMoveSpeed);
-                    physicsVelocity.Linear.xyz = movement;
                     
+                    physicsVelocity.Linear.xyz = movement;
                     physicsVelocity.Linear.xyz = new float3
                     (
                         Mathf.Clamp(physicsVelocity.Linear.x, -_clampValue, _clampValue),
